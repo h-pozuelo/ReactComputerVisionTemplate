6e66bfec4ae837252c4608384e68997918ba67e6
h-pozuelo <122695697+h-pozuelo@users.noreply.github.com>
2023-05-31 22:48:51 +0200

    1. Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,110 @@
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+// 1. TODO - Import required model here
+// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+import Webcam from "react-webcam";
+import "./App.css";
+// 2. TODO - Import drawing utility here
+// e.g. import { drawRect } from "./utilities";
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network 
+    // e.g. const net = await cocossd.load();
+    const net = await tf.loadGraphModel(`${window.location.href}/yolov8_custom_web_model/model.json`);
+
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 2000);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      // e.g. const obj = await net.detect(video);
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [640, 640]);
+      const casted = resized.cast('float32');
+      const expanded = casted.expandDims(0);
+      const obj = net.execute(expanded);
+
+      console.log(obj);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+
+      // 5. TODO - Update drawing utility
+      // drawSomething(obj, ctx)  
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
+    }
+  };
+
+  useEffect(() => { runCoco() }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
